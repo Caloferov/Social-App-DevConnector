3f41768156e6254f9702dc99a221cd9b5c8dd724
kirkalof <kiril.kaloferov@bulpros.com>
2019-04-16 15:44:49 +0300

    JWT token funcionality, current route added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://caloferov:Monmm99?@ds131753.mlab.com:31753/socialappdb'
+  mongoURI: 'mongodb://caloferov:Monmm99?@ds131753.mlab.com:31753/socialappdb',
+  secretOrKey: 'secret'
 };
